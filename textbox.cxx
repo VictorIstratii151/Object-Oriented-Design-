9c3b94ccdcdacd84f4d5b355befc7cedb5de417a
VictorIstratii151 <victor.istratii@faf.utm.md>
2018-02-28 20:48:15 +0200

    Defined the methods for setting up message textbox in the game window and integrated it with other classes

---
+++ textbox.cxx
@@ -0,0 +1,65 @@
+#include "textbox.h"
+
+Textbox::Textbox()
+{
+    Setup(5, 9, 200, sf::Vector2f(0, 0));
+}
+
+Textbox::Textbox(int visible, int charSize, int width, sf::Vector2f screenPos)
+{
+    Setup(visible, charSize, width, screenPos);
+}
+
+Textbox::~Textbox()
+{
+    Clear();
+}
+
+void Textbox::Setup(int visible, int charSize, int width, sf::Vector2f screenPos)
+{
+    numVisible = visible;
+
+    sf::Vector2f offset(2.0f, 2.0f);
+
+    font.loadFromFile("arial.ttf");
+    content.setFont(font);
+    content.setString("");
+    content.setCharacterSize(charSize);
+    content.setColor(sf::Color::White);
+    content.setPosition(screenPos + offset);
+
+    backdrop.setSize(sf::Vector2f(width, (visible * (charSize * 1.2f))));
+    backdrop.setFillColor(sf::Color(90, 90, 90, 90));
+    backdrop.setPosition(screenPos);
+}
+
+void Textbox::Add(std::string message)
+{
+    messages.push_back(message);
+    if(messages.size() > 5)
+    {
+        messages.erase(messages.begin());
+    }
+}
+
+void Textbox::Clear()
+{
+    messages.clear();
+}
+
+void Textbox::Render(sf::RenderWindow & window)
+{
+    std::string displayContent;
+
+    for(auto itr : messages)
+    {
+        displayContent.append(itr + "\n");
+    }
+
+    if(displayContent != "")
+    {
+        content.setString(displayContent);
+        window.draw(backdrop);
+        window.draw(content);
+    }
+}
