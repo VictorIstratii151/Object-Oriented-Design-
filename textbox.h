dfe3f27113d89631d4a6dae08f4345583e5ef429
VictorIstratii151 <victor.istratii@faf.utm.md>
2018-02-28 20:46:46 +0200

    Added a helper class to show game-related messages

---
+++ textbox.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+using MessageContainer = std::vector<std::string>;
+
+class Textbox
+{
+public:
+    Textbox();
+    Textbox(int visible, int charSize, int width, sf::Vector2f screenPos);
+    ~Textbox();
+
+    void Setup(int visible, int charSize, int width, sf::Vector2f screenPos);
+    void Add(std::string message);
+    void Clear();
+    void Render(sf::RenderWindow & window);
+
+private:
+    MessageContainer messages;
+    int numVisible;
+    sf::RectangleShape backdrop;
+    sf::Font font;
+    sf::Text content;
+};
