343d1f89b7b475cc466bbb4844e9b6ef590d87e1
VictorIstratii151 <victor.istratii@faf.utm.md>
2018-02-28 20:37:54 +0200

    Created the snake class header file

---
+++ snake.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "textbox.h"
+
+using namespace std;
+
+struct SnakeSegment
+{
+    SnakeSegment(int x, int y) : position(x, y)
+    {
+
+    }
+
+    sf::Vector2i position;
+};
+ 
+using SnakeContainer = vector<SnakeSegment>;
+
+enum class Direction{ None, Up, Down, Left, Right };
+
+class Snake
+{
+public:
+    Snake(int blockSize, Textbox * textbox);
+    ~Snake();
+
+    void SetDirection(Direction dir);
+    Direction GetDirection() const;
+    Direction GetPhysicalDirection();
+    sf::Vector2i GetPosition() const;
+    int GetSpeed() const;
+    int GetLives() const;
+    int GetScore() const;
+    void IncreaseScore();
+    bool HasLost() const;
+
+    void Lose();
+    void ToggleLost();
+
+    void Extend();
+    void Reset();
+
+    void Move();
+    void Tick();
+    void Cut(int segments);
+    void Render(sf::RenderWindow & window);
+
+private:
+    void CheckCollision();
+
+    SnakeContainer snakeBody;
+    Direction dir;
+    int size;
+    int speed;
+    int lives;
+    int score;
+    bool lost;
+    sf::RectangleShape bodyRect;
+    Textbox * log;
+};
+
